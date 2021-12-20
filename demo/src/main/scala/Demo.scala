cd00403583f3fedbcfb29f71e8607488879f705e
Michele Rastelli <michele@arangodb.com>
2021-12-20 12:01:31 +0100

    fixed Spark 2.4 compatibility

--- demo/src/main/scala/Demo.scala
+++ demo/src/main/scala/Demo.scala
@@ -5,7 +5,7 @@ object Demo {
   val password: String = System.getProperty("password", "test")
   val endpoints: String = System.getProperty("endpoints", "172.17.0.1:8529,172.17.0.1:8539,172.17.0.1:8549")
   val sslEnabled: String = System.getProperty("ssl.enabled", "false")
-  val sslCertValue: String = System.getProperty("ssl.cert.value")
+  val sslCertValue: String = System.getProperty("ssl.cert.value", "")
 
   val spark: SparkSession = SparkSession.builder
     .appName("arangodb-demo")
