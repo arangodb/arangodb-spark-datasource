6edcbac6c6b412fc2ce4d3a2ccafb1ce6941a0fb
Michele Rastelli <michele@arangodb.com>
2022-02-18 10:47:28 +0100

    spark remote

--- demo/src/main/scala/Demo.scala
+++ demo/src/main/scala/Demo.scala
@@ -1,7 +1,9 @@
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
+
 object Demo {
-  val importPath: String = System.getProperty("importPath", "/demo/docker/import")
+  val importPath: String = System.getProperty("importPath", "/import")
   val password: String = System.getProperty("password", "test")
   val endpoints: String = System.getProperty("endpoints", "172.17.0.1:8529,172.17.0.1:8539,172.17.0.1:8549")
   val sslEnabled: String = System.getProperty("ssl.enabled", "false")
@@ -9,7 +11,11 @@ object Demo {
 
   val spark: SparkSession = SparkSession.builder
     .appName("arangodb-demo")
-    .master("local[*, 3]")
+    .master("spark://spark-master:7077")
+    .config(new SparkConf()
+      .setJars(Seq("lib/arangodb-spark-datasource-3.1_2.12-1.0.0-jar-with-dependencies.jar"))
+//      .setJars(Seq("https://repo1.maven.org/maven2/com/arangodb/arangodb-spark-datasource-3.1_2.12/1.0.0/arangodb-spark-datasource-3.1_2.12-1.0.0-jar-with-dependencies.jar"))
+    )
     .getOrCreate
 
   val options = Map(
