fa003f1d214769ab7e5fb462e74f90565bdd9541
Michele Rastelli <michele@arangodb.com>
2022-02-28 14:07:53 +0100

    [DE-172] Bugfix exceptions serialization (#15)

--- arangodb-spark-commons/src/main/scala/org/apache/spark/sql/arangodb/commons/exceptions/ArangoDBMultiException.scala
+++ arangodb-spark-commons/src/main/scala/org/apache/spark/sql/arangodb/commons/exceptions/ArangoDBMultiException.scala
@@ -3,10 +3,15 @@ package org.apache.spark.sql.arangodb.commons.exceptions
 import com.arangodb.entity.ErrorEntity
 import org.apache.spark.sql.arangodb.commons.exceptions.ArangoDBMultiException.convert
 
-class ArangoDBMultiException(val errors: Iterable[ErrorEntity]) extends RuntimeException(convert(errors))
+// Due to https://github.com/scala/bug/issues/10679 scala Stream serialization could generate StackOverflowError. To
+// avoid it we use:
+//    val errors: Array[ErrorEntity]
+// instead of:
+//    val errors: Iterable[ErrorEntity]
+class ArangoDBMultiException(val errors: Array[ErrorEntity]) extends RuntimeException(convert(errors))
 
 private object ArangoDBMultiException {
-  private def convert(errors: Iterable[ErrorEntity]): String = errors
+  private def convert(errors: Array[ErrorEntity]): String = errors
     .map(it => s"""Error: ${it.getErrorNum} - ${it.getErrorMessage}""")
     .mkString("[\n\t", ",\n\t", "\n]")
 }
---
+++ arangodb-spark-commons/src/test/scala/org/apache/spark/sql/arangodb/commons/exceptions/ExceptionsSerializationTest.scala
@@ -0,0 +1,28 @@
+package org.apache.spark.sql.arangodb.commons.exceptions
+
+import com.arangodb.entity.ErrorEntity
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Test
+
+import java.io.{ByteArrayOutputStream, ObjectOutputStream}
+
+class ExceptionsSerializationTest {
+
+  @Test
+  def arangoDBMultiException(): Unit = {
+    val mapper = new ObjectMapper()
+    val errors = Stream.range(1, 10000).map(_ =>
+      mapper.readValue(
+        """{
+          |"errorMessage": "errorMessage",
+          |"errorNum": 1234
+          |}""".stripMargin, classOf[ErrorEntity])
+    )
+    val e = new ArangoDBMultiException(errors.toArray)
+    val objectOutputStream = new ObjectOutputStream(new ByteArrayOutputStream())
+    objectOutputStream.writeObject(e)
+    objectOutputStream.flush()
+    objectOutputStream.close()
+  }
+
+}
