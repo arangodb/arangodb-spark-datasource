89443f0e8b1e0e7d3bc3a16cd0a4650864eda3fc
Michele Rastelli <michele@arangodb.com>
2022-05-24 12:43:49 +0200

    [DE-136] Scala 2.13 support (#32)
    * scala 2.13 code compatibility
    
    * scala 2.13 maven profile
    
    * scala 2.13 CI
    
    * import fixes
    
    * fixed demo

--- integration-tests/src/test/scala/org/apache/spark/sql/arangodb/datasource/write/CreateCollectionTest.scala
+++ integration-tests/src/test/scala/org/apache/spark/sql/arangodb/datasource/write/CreateCollectionTest.scala
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
-import scala.jdk.CollectionConverters.seqAsJavaListConverter
+import scala.collection.JavaConverters._
 
 
 class CreateCollectionTest extends BaseSparkTest {
