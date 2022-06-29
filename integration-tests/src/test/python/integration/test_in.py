7697626f8c46b47f1610679e123e777c98bb4ac3
Alex Geenen <ageenen@pm.me>
2022-06-29 14:15:41 +0200

    (test): Add equalTo & IN Tests Python

--- integration-tests/src/test/python/integration/test_in.py
+++ integration-tests/src/test/python/integration/test_in.py
@@ -13,15 +13,10 @@ from integration import test_basespark
 data = [
     {
         "bool": False,
-        "double": 1.1,
-        "float": 0.09375,
         "integer": 1,
-        "long": 1,
         "date": date.fromisoformat("2021-01-01").isoformat(),
         "timestampString": datetime.fromisoformat("2021-01-01 01:01:01.111").isoformat(" "),
         "timestampMillis": datetime.fromisoformat("2021-01-01 01:01:01.111").timestamp(),
-        "short": 1,
-        "byte": 1,
         "string": "one",
         "intArray": [1, 1, 1],
         "struct": {
@@ -31,15 +26,10 @@ data = [
     },
     {
         "bool": True,
-        "double": 2.2,
-        "float": 2.2,
         "integer": 2,
-        "long": 2,
         "date": date.fromisoformat("2022-02-02").isoformat(),
         "timestampString": datetime.fromisoformat("2022-02-02 02:02:02.222").isoformat(" "),
         "timestampMillis": datetime.fromisoformat("2022-02-02 02:02:02.222").timestamp(),
-        "short": 2,
-        "byte": 2,
         "string": "two",
         "intArray": [2, 2, 2],
         "struct": {
@@ -51,15 +41,10 @@ data = [
 
 schema = StructType([
     StructField("bool", BooleanType(), nullable=False),
-    StructField("double", DoubleType(), nullable=False),
-    StructField("float", FloatType(), nullable=False),
     StructField("integer", IntegerType(), nullable=False),
-    StructField("long", LongType(), nullable=False),
     StructField("date", DateType(), nullable=False),
     StructField("timestampString", TimestampType(), nullable=False),
     StructField("timestampMillis", TimestampType(), nullable=False),
-    StructField("short", ShortType(), nullable=False),
-    StructField("byte", ByteType(), nullable=False),
     StructField("string", StringType(), nullable=False),
     StructField("intArray", ArrayType(IntegerType()), nullable=False),
     StructField("struct", StructType([
@@ -70,43 +55,53 @@ schema = StructType([
 
 
 @pytest.fixture(scope="module")
-def eq_df(database_conn: arango.database.StandardDatabase, spark: SparkSession):
-    df = test_basespark.create_df(database_conn, spark, "equalTo", data, schema)
+def in_df(database_conn: arango.database.StandardDatabase, spark: SparkSession):
+    df = test_basespark.create_df(database_conn, spark, "in", data, schema)
     yield df
-    test_basespark.drop_table(database_conn, "equalTo")
+    test_basespark.drop_table(database_conn, "in")
 
 
-def test_bool(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
-    verify_field_eq("bool", spark, eq_df)
+def test_bool(spark: SparkSession, in_df: pyspark.sql.DataFrame):
+    field_name = "bool"
+
+    res = in_df.filter(col(field_name).isin(True, False)).collect()
+    assert len(res) == 2
+
+    sql_res = spark.sql(f"""
+    SELECT * FROM in
+    WHERE {field_name} IN (true, false)
+    """).collect()
+    assert len(sql_res) == 2
 
 
-def test_double(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
-    verify_field_eq("double", spark, eq_df)
+def test_integer(spark: SparkSession, in_df: pyspark.sql.DataFrame):
+    field_name = "integer"
+    value = data[0][field_name]
+
+    res = in_df.filter(col(field_name).isin(0, 1)).collect()
+    assert len(res) == 1
+    assert res[0].asDict()[field_name] == value
+
+    sql_res = spark.sql(f"""
+        SELECT * FROM in
+        WHERE {field_name} IN (0, 1)
+        """).collect()
+    assert len(sql_res) == 1
+    assert sql_res[0].asDict()[field_name] == value
 
 
-def test_float(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
-    verify_field_eq("float", spark, eq_df)
-
-
-def test_integer(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
-    verify_field_eq("integer", spark, eq_df)
-
-
-def test_long(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
-    verify_field_eq("long", spark, eq_df)
-
-
-def test_date(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
+def test_date(spark: SparkSession, in_df: pyspark.sql.DataFrame):
     field_name = "date"
     value = data[0][field_name]
-    res = eq_df.filter(col(field_name) == value).collect()
+    value2 = date.fromisoformat("2020-01-01").isoformat()
+    res = in_df.filter(col(field_name).isin(value, value2)).collect()
 
     assert len(res) == 1
     assert res[0].asDict()[field_name] == date.fromisoformat(value)
 
     sql_res = spark.sql(f"""
-        SELECT * FROM equalTo
-        WHERE {field_name} = "{value}"
+        SELECT * FROM in
+        WHERE {field_name} IN ("{value}", "{value2}")
         """).collect()
 
     assert len(sql_res) == 1
@@ -115,17 +110,19 @@ def test_date(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
 
 # FIXME: pyspark - python timestamp comparison
 @pytest.mark.xfail
-def test_timestamp_string(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
+def test_timestamp_string(spark: SparkSession, in_df: pyspark.sql.DataFrame):
     field_name = "timestampString"
     value = data[0][field_name]
-    res = eq_df.filter(col(field_name) == to_timestamp(lit(value))).collect()
+    value2 = datetime.fromisoformat("2020-01-01 00:00:00.000").isoformat(" ")
+
+    res = in_df.filter(col(field_name).isin(value, value2)).collect()
 
     assert len(res) == 1
     assert res[0].asDict()[field_name] == value
 
     sql_res = spark.sql(f"""
-        SELECT * FROM equalTo
-        WHERE {field_name} = "{value}"
+        SELECT * FROM in
+        WHERE {field_name} IN ("{value}", "{value2}")
         """).collect()
 
     assert len(sql_res) == 1
@@ -134,59 +131,54 @@ def test_timestamp_string(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
 
 # FIXME: pyspark - python timestamp comparison
 @pytest.mark.xfail
-def test_timestamp_millis(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
+def test_timestamp_millis(spark: SparkSession, in_df: pyspark.sql.DataFrame):
     field_name = "timestampMillis"
-    value = data[0][field_name]
-    res = eq_df.filter(col(field_name) == to_timestamp(lit(value))).collect()
+    value = datetime.fromisoformat("2021-01-01 01:01:01.111").timestamp()
+    value2 = datetime.fromisoformat("2020-01-01 00:00:00.000").timestamp()
+    res = in_df.filter(col(field_name).isin(value, value2)).collect()
 
     assert len(res) == 1
     assert res[0].asDict()[field_name] == value
 
     sql_res = spark.sql(f"""
-        SELECT * FROM equalTo
-        WHERE {field_name} = "{value}"
+        SELECT * FROM in
+        WHERE {field_name} IN ("{value}", "{value2}")
         """).collect()
 
     assert len(sql_res) == 1
     assert sql_res[0][field_name] == value
 
 
-def test_short(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
-    verify_field_eq("short", spark, eq_df)
-
-
-def test_byte(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
-    verify_field_eq("byte", spark, eq_df)
-
-
-def test_string(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
+def test_string(spark: SparkSession, in_df: pyspark.sql.DataFrame):
     field_name = "string"
     value = data[0][field_name]
-    res = eq_df.filter(col(field_name) == value).collect()
+    value2 = "foo"
+    res = in_df.filter(col(field_name).isin(value, value2)).collect()
 
     assert len(res) == 1
     assert res[0].asDict()[field_name] == value
 
     sql_res = spark.sql(f"""
-        SELECT * FROM equalTo
-        WHERE {field_name} = "{value}"
+        SELECT * FROM in
+        WHERE {field_name} IN ("{value}", "{value2}")
         """).collect()
 
     assert len(sql_res) == 1
     assert sql_res[0][field_name] == value
 
 
-def test_int_array(spark: SparkSession, eq_df: pyspark.sql.DataFrame):
+def test_int_array(spark: SparkSession, in_df: pyspark.sql.DataFrame):
     field_name = "intArray"
     value = data[0][field_name]
-    res = eq_df.filter(col(field_name) == array(*[lit(x) for x in value])).collect()
+    value2 = [4, 5, 6]
+    res = in_df.filter(col(field_name).isin(array(*[lit(x) for x in value]), array(*[lit(x) for x in value2]))).collect()
 
     assert len(res) == 1
     assert res[0][field_name] == value
 
     sql_res = spark.sql(f"""
-    SELECT * FROM equalTo
-    WHERE {field_name} = array({",".join([str(x) for x in value])})
+    SELECT * FROM in
+    WHERE {field_name} IN (array({",".join([str(x) for x in value])}), array({",".join([str(x) for x in value2])}))
     """).collect()
 
     assert len(sql_res) == 1
@@ -198,24 +190,8 @@ def test_struct(spark: SparkSession):
     value = Row(a="a1", b=1)
 
     sql_res = spark.sql(f"""
-    SELECT * FROM equalTo
-    WHERE {field_name} = struct("a1" AS a, 1 AS b)
+    SELECT * FROM in
+    WHERE {field_name} IN (("foo" AS a, 9 as b), ("a1" AS a, 1 AS b))
     """).collect()
-    assert len(sql_res) == 1
-    assert sql_res[0][field_name] == value
-
-
-def verify_field_eq(field_name: str, spark: SparkSession, eq_df: pyspark.sql.DataFrame):
-    value = data[0][field_name]
-    res = eq_df.filter(col(field_name) == value).collect()
-
-    assert len(res) == 1
-    assert res[0].asDict()[field_name] == value
-
-    sql_res = spark.sql(f"""
-        SELECT * FROM equalTo
-        WHERE {field_name} = {value}
-        """).collect()
-
     assert len(sql_res) == 1
     assert sql_res[0][field_name] == value
