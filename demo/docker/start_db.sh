c71df2e8d06d0080a6aef5a77824fb351d6f3973
Michele Rastelli <michele@arangodb.com>
2024-06-27 14:45:13 +0200

    CI: CircleCI

--- demo/docker/start_db.sh
+++ demo/docker/start_db.sh
@@ -3,8 +3,8 @@
 # Configuration environment variables:
 #   STARTER_MODE:             (single|cluster|activefailover), default single
 #   DOCKER_IMAGE:             ArangoDB docker image, default docker.io/arangodb/arangodb:latest
+#   STARTER_DOCKER_IMAGE:     ArangoDB Starter docker image, default docker.io/arangodb/arangodb-starter:latest
 #   SSL:                      (true|false), default false
-#   DATABASE_EXTENDED_NAMES:  (true|false), default false
 #   ARANGO_LICENSE_KEY:       only required for ArangoDB Enterprise
 
 # EXAMPLE:
@@ -12,10 +12,10 @@
 
 STARTER_MODE=${STARTER_MODE:=single}
 DOCKER_IMAGE=${DOCKER_IMAGE:=docker.io/arangodb/arangodb:latest}
+STARTER_DOCKER_IMAGE=${STARTER_DOCKER_IMAGE:=docker.io/arangodb/arangodb-starter:latest}
 SSL=${SSL:=false}
-DATABASE_EXTENDED_NAMES=${DATABASE_EXTENDED_NAMES:=false}
+COMPRESSION=${COMPRESSION:=false}
 
-STARTER_DOCKER_IMAGE=docker.io/arangodb/arangodb-starter:latest
 GW=172.28.0.1
 docker network create arangodb --subnet 172.28.0.0/16
 
@@ -26,9 +26,6 @@ docker pull $STARTER_DOCKER_IMAGE
 docker pull $DOCKER_IMAGE
 
 LOCATION=$(pwd)/$(dirname "$0")
-
-echo "Averysecretword" > "$LOCATION"/jwtSecret
-docker run --rm -v "$LOCATION"/jwtSecret:/jwtSecret "$STARTER_DOCKER_IMAGE" auth header --auth.jwt-secret /jwtSecret > "$LOCATION"/jwtHeader
 AUTHORIZATION_HEADER=$(cat "$LOCATION"/jwtHeader)
 
 STARTER_ARGS=
@@ -41,36 +38,35 @@ if [ "$STARTER_MODE" == "single" ]; then
 fi
 
 if [ "$SSL" == "true" ]; then
-    STARTER_ARGS="$STARTER_ARGS --ssl.keyfile=server.pem"
+    STARTER_ARGS="$STARTER_ARGS --ssl.keyfile=/data/server.pem"
     SCHEME=https
     ARANGOSH_SCHEME=http+ssl
 fi
 
-if [ "$DATABASE_EXTENDED_NAMES" == "true" ]; then
-    STARTER_ARGS="${STARTER_ARGS} --all.database.extended-names-databases=true"
+if [ "$COMPRESSION" == "true" ]; then
+    STARTER_ARGS="${STARTER_ARGS} --all.http.compress-response-threshold=1"
 fi
 
-if [ "$USE_MOUNTED_DATA" == "true" ]; then
-    STARTER_ARGS="${STARTER_ARGS} --starter.data-dir=/data"
-    MOUNT_DATA="-v $LOCATION/data:/data"
-fi
+# data volume
+docker create -v /data --name arangodb-data alpine:3 /bin/true
+docker cp "$LOCATION"/jwtSecret arangodb-data:/data
+docker cp "$LOCATION"/server.pem arangodb-data:/data
 
 docker run -d \
     --name=adb \
     -p 8528:8528 \
-    -v "$LOCATION"/server.pem:/server.pem \
-    -v "$LOCATION"/jwtSecret:/jwtSecret \
-    $MOUNT_DATA \
+    --volumes-from arangodb-data \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e ARANGO_LICENSE_KEY="$ARANGO_LICENSE_KEY" \
     $STARTER_DOCKER_IMAGE \
     $STARTER_ARGS \
     --docker.net-mode=default \
     --docker.container=adb \
-    --auth.jwt-secret=/jwtSecret \
+    --auth.jwt-secret=/data/jwtSecret \
     --starter.address="${GW}" \
     --docker.image="${DOCKER_IMAGE}" \
-    --starter.local --starter.mode=${STARTER_MODE} --all.log.level=debug --all.log.output=+ --log.verbose
+    --starter.local --starter.mode=${STARTER_MODE} --all.log.level=debug --all.log.output=+ --log.verbose \
+    --all.server.descriptors-minimum=1024 --all.javascript.allow-admin-execute=true
 
 
 wait_server() {
@@ -99,6 +95,13 @@ for a in ${COORDINATORS[*]} ; do
     echo ""
     echo "Requesting endpoint version..."
     curl -u root:test --insecure --fail "$SCHEME://$a/_api/version"
+done
+
+echo ""
+echo ""
+echo "Copying test ML models into containers..."
+for c in $(docker ps -a -f name=adb-.* -q) ; do
+    docker cp "$LOCATION"/foo.bin "$c":/tmp
 done
 
 echo ""
