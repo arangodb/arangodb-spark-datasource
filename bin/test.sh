5cd8b098284bce66c101188468e665e38f7a6005
Michele Rastelli <michele@arangodb.com>
2026-01-09 17:54:40 +0100

    support Spark 4.1

--- bin/test.sh
+++ bin/test.sh
@@ -18,3 +18,6 @@ mvn test -Pspark-3.5 -Pscala-2.13
 
 mvn clean -Pspark-4.0 -Pscala-2.13.18
 mvn test -Pspark-4.0 -Pscala-2.13.18
+
+mvn clean -Pspark-4.1 -Pscala-2.13.18
+mvn test -Pspark-4.1 -Pscala-2.13.18
