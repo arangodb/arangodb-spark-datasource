d1067eb9a570e46067eeb733b3e39c8061967e0c
Michele Rastelli <michele@arangodb.com>
2026-01-09 19:03:14 +0100

    [DE-1108] Support Spark 4.1 (#69)

--- bin/clean.sh
+++ bin/clean.sh
@@ -5,3 +5,4 @@ mvn clean -Pspark-3.4 -Pscala-2.13
 mvn clean -Pspark-3.5 -Pscala-2.12
 mvn clean -Pspark-3.5 -Pscala-2.13
 mvn clean -Pspark-4.0 -Pscala-2.13.18
+mvn clean -Pspark-4.1 -Pscala-2.13.18
