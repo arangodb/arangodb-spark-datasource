445d345fb70bb79108548c366c4f37e5fb4459c9
Michele Rastelli <michele@arangodb.com>
2026-01-09 22:04:05 +0100

    dropped support to Spark 3.4

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mvn clean -Pspark-3.4 -Pscala-2.12
-mvn clean -Pspark-3.4 -Pscala-2.13
 mvn clean -Pspark-3.5 -Pscala-2.12
 mvn clean -Pspark-3.5 -Pscala-2.13
 mvn clean -Pspark-4.0 -Pscala-2.13.18
